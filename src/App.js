b02b9c3f6c98c7cc05498b8cfa6415a0d2a9ae40
ash13y16 <ashley20854@gmail.com>
2024-11-19 16:08:44 -0600

    Title added

--- src/App.js
+++ src/App.js
@@ -3,22 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <header className="App-header">
+              <h1>Roommate</h1>
+              <h2>Finder</h2>
+              <div id="scroll">
+                  <p className="scroll">Scroll</p>
+                  <p style={{margin: 0, padding: 0}}>↓</p>
+              </div>
+
+          </header>
+          <br/>
+          <br/>
+          <br/>
+          <p>hehe got u</p>
+      </div>
   );
 }
 
