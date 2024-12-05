5e0dd53f86b25f69db06970eada2f6629ca1b493
ash13y16 <ashley20854@gmail.com>
2024-12-05 16:52:25 -0600

    page 2

--- src/Pages/Page1.js
+++ src/Pages/Page1.js
@@ -1,3 +1,9 @@
 export function Page1() {
+    return (
+        <div class={"Page1"}>
+            <h1>Page 1</h1>
+            <h3>This is where the mc questions can go</h3>
+        </div>
 
+    );
 }
